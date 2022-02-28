dc9b18c57aa87736aabf5dcb28c7fd3792dec63e
Marina Khadieva <utabond89@gmail.com>
2022-02-28 17:50:38 +0300

    Почти доделала

--- js/calc_browser.js
+++ js/calc_browser.js
@@ -9,7 +9,30 @@ const btnEquals = document.querySelector('.equals')
 let num1 = "";
 let num2 = "";
 let operator = "";
-let result = 0;
+let result = "";
+
+
+function Calc(op, num1, num2){
+	switch (op){
+		case'div': result = num1/num2;
+		break;
+		case 'sum': result = num1 + num2;
+		break;
+		case 'sub': result = num1 - num2;
+		break;
+		case 'multi': result = num1 * num2;
+		break;
+	}
+	 return result;
+  }
+
+/* let operations = {
+	'sum': num1 + num2,
+	'sub': num1 - num2,
+	'multi': num1 * num2,
+	'div': num1 / num2,
+	//'div': (num2 !== 0) ? num1 / num2 : "Err",
+} */
 
 
 
@@ -17,7 +40,7 @@ let result = 0;
 for (let btnNumber of btnsNumber) {
 	btnNumber.addEventListener('click', function (event) {
 		const btnValue = event.target.textContent;
-		if ((calcOutput.textContent === "0") || (num1 === "" && operator === "")) {
+		if ((calcOutput.textContent === "0") || (operator !== "")) {
 			calcOutput.textContent = btnValue;
 		} else {
 			calcOutput.textContent += btnValue;
@@ -28,6 +51,9 @@ for (let btnNumber of btnsNumber) {
 }
 btnClear.addEventListener('click', function () {
 	calcOutput.textContent = "0";
+	operator = "";
+	num1 = "";
+	num2 = "";
 })
 
 btnBackSpace.addEventListener('click', function () {
@@ -44,37 +70,30 @@ btnBackSpace.addEventListener('click', function () {
 for (let btnOperator of btnsOperator) {
 	btnOperator.addEventListener('click', function (event) {
 		if (operator !== "") {
-			num2 = + calcOutput.textContent
-			num1 = operations[operator];
+			num2 = +calcOutput.textContent
+			num1 = Calc(operator, num1, num2);
 			operator = event.target.id;
 			calcOutput.textContent = num1;
 		} else {
 			num1 = +calcOutput.textContent;
 			operator = event.target.id;
 		}
-
 	})
 }
 
 btnEquals.addEventListener('click', function () {
 	if (operator !== "") {
 		num2 = +calcOutput.textContent;
-		result = operations[operator];
+		Calc(operator, num1, num2);
 		calcOutput.textContent = result;
 		operator = "";
-		num1 = result;
+		num1 = "";
 	} else {
 		alert("Выберите математическое действие")
 	}
 
 })
 
-let operations = {
-	'sum': num1 + num2,
-	'sub': num1 - num2,
-	'multi': num1 * num2,
-	'div': (num2 !== 0) ? num1 / num2 : "Err",
-}
 
 
 
