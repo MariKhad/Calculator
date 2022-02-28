e24fbad93b1eef34655b67cba87afb9feddc588b
Marina Khadieva <utabond89@gmail.com>
2022-02-28 13:08:36 +0300

    1

--- js/calc_browser.js
+++ js/calc_browser.js
@@ -1,11 +1,12 @@
 addEventListener("DOMContentLoaded", Calc);
+
 let num1, num2, operator;
-let outputValue = "0";
+let outputValue = "1234";
 document.querySelector('.calc__output').textContent = outputValue;
 
 
 Calc() {
-	let numbers = document.querySelectorAll('.num');
+	let numbers = document.querySelectorAll('.number');
 	for (let number of numbers) {
 		number.addEventListener('click', function {
 			outputValue = outputValue + number.textContent;
@@ -17,7 +18,7 @@ Calc() {
 
 let operations = {
 	'sum': num1 + num2,
-	'sub': num1 - num2, dd
+	'sub': num1 - num2,
 	'multi': num1 * num2,
 	'div': (num2 !== 0) ? num1 / num2 : "Error",
 }
